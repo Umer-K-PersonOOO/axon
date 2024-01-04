c0d0c7c755659dfafb22dc71cca12dd8426d5d0f
PersonOOO <umerkhan5110@gmail.com>
2024-01-04 14:48:15 -0600

    reset

--- src/components/ContactPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Footer from "./Footer";
-const ContactPage: React.FC = () => {
-  return <Footer />;
-};
-
-export default ContactPage;
